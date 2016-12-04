e739594c14646ebb8aaf432cee46455f0ce6aa93
Abigail Watson <awatson1978@gmail.com>
2016-12-03 21:00:32 -0600

    circle.yml

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name:    "clinical:env",
-  version: "3.2.2",
+  version: "3.3.0",
   summary: "Set environment variables defined in a .env file",
   git:     "https://github.com/clinical-meteor/clinical-env",
   contributors: [
@@ -25,23 +25,14 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('meteorhacks:inject-initial@1.0.2');
-  api.use('underscore@1.0.3', ['server']);
+  api.use('underscore@1.0.10', ['server']);
 
-  api.imply('meteorhacks:inject-initial@1.0.2');
+  //api.imply('meteorhacks:inject-initial@1.0.2');
 
   api.addFiles('lib/clinical-env.js', ['client', 'server']);
   api.addFiles('lib/clinical-env-client.js', ['client']);
   api.addFiles('lib/clinical-env-server.js', ['server']);
 
-  api.export('process', ['client']);
+  // api.export('process', ['client']);
   api.export('Env');
-});
-
-
-
-Package.on_test(function (api) {
-  api.use('meteor-platform@1.1.3');
-  api.use('clinical:env');
-  api.use('clinical:verification');
-  api.addFiles('tests/tinytests/clinical-env.js');
 });
